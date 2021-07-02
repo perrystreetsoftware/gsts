2eed9c6ff5022f735af5a0f302541fdfbd702b64
Eric Silverberg <eric@perrystreet.com>
2021-07-02 16:06:11 -0400

    Semver sort has bugs. Use server directly.

--- endpointVerification.js
+++ endpointVerification.js
@@ -3,8 +3,8 @@
 const fs = require('fs');
 const fsPromises = fs.promises;
 const untildify = require('untildify');
-const semverSort = require('semver-sort');
 const path = require('path');
+const semver = require('semver')
 
 async function huntForExtension() {
   let extensionsDirectory = untildify('~/Library/Application Support/Google/Chrome/Default/Extensions/');
@@ -14,7 +14,10 @@ async function huntForExtension() {
   return fsPromises.readdir(endpointVerificationExtensionDirectory)
     .then(function(files) {
       if (files && files.length > 0) {
-        latestExtensionVersion = semverSort.desc(files)[0];
+
+        latestExtensionVersion = files.sort((x, y) => {
+          return semver.compare(semver.coerce(y), semver.coerce(x));
+        })[0];
 
         console.log("\nENDPOINT VERIFICATION ENABLED")
         console.log(`Found Endpoint Verification Extension version installed: ${latestExtensionVersion}`)
@@ -24,7 +27,7 @@ async function huntForExtension() {
         return null;
       }
     }).catch((error) => {
-      console.warn("\nWARNING")
+      console.warn(`\nWARNING: ${error}`)
       console.warn(`Endpoint verification requested but plugin not found at ${endpointVerificationExtensionDirectory}`)
       console.warn("Please make sure you have installed the Google Endpoint Verification Chrome Extension at: https://chrome.google.com/webstore/detail/endpoint-verification/callobklhcbilhphinckomhgkigmfocg?hl=en")
 
